40201cdfb7b5a4eccb80a3a9ab4f539d33c50882
Sharjanan <98242877+Sharjanan@users.noreply.github.com>
2025-05-27 16:05:50 -0400

    Fixed a bug #1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import ClientLayout from "@/components/ClientLayout";
-
+import ClientLayout from "@/components/ClientLayout"; // This should be a client component
 
 export const metadata: Metadata = {
   title: "WavyCrown",
@@ -13,9 +11,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className="bg-black text-white">
-        <ClientLayout> 
-
-          {children}</ClientLayout>
+        <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
   );
